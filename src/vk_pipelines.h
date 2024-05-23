2c2ce25d4fac4db95836e28468268124ce9edd7f
Ben Schwarz <100788839+benblack90@users.noreply.github.com>
2024-05-23 15:03:14 +0100

    semi-transparent monkey via additive blend

--- src/vk_pipelines.h
+++ src/vk_pipelines.h
@@ -36,5 +36,7 @@ public:
     void set_depth_format(VkFormat format);
     void disable_depthtest();
     void enable_depthtest(bool depthWriteEnable, VkCompareOp op);
+    void enable_blending_alphablend();
+    void enable_blending_additive();
 };
 
